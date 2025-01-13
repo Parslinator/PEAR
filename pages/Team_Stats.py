eb07ca139f5d8ff653e26e5629b974d0a774998d
Parslinator <wparsley2002@gmail.com>
2025-01-13 11:17:15 -0600

    changing widths

--- pages/Team_Stats.py
+++ pages/Team_Stats.py
@@ -132,23 +132,15 @@ if os.path.exists(folder_path):
 
     cols = st.columns(2)
     with cols[0]:
-        st.markdown(
-            f"""
-            <div style="text-align: center;">
-                <img src="data:image/png;base64,{power_rating_team_pyramid}", width="500" alt="Ratings Team Pyramid">
-                <p>Ratings Team Pyramid</p>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
+        st.image(power_rating_team_pyramid, width = 500, caption="Power Ratings Team Pyramid")
     with cols[1]:
         st.image(projected_playoff, use_container_width=True, caption="Ratings Projected Playoff - Conference Champions are Highest Rated Team")
 
     st.subheader("Rankings Information")
-    st.image(most_deserving, width=1500, caption="Most Deserving Rankings")
+    st.image(most_deserving, width=1000, caption="Most Deserving Rankings")
     cols = st.columns(2)
     with cols[0]:
-        st.image(most_deserving_team_pyramid, use_container_width=True, caption="Most Deserving Team Pyramid")
+        st.image(most_deserving_team_pyramid, width=500, caption="Most Deserving Team Pyramid")
     with cols[1]:
         st.image(most_deserving_playoff, use_container_width=True, caption="Rankings Projected Playoff")
 
