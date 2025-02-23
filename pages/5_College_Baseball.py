ea9929ca36b470ac5a15cd6d6020259cd6413d85
Parslinator <wparsley2002@gmail.com>
2025-02-22 20:36:15 -0600

    updating caption

--- pages/5_College_Baseball.py
+++ pages/5_College_Baseball.py
@@ -390,7 +390,7 @@ def find_spread(home_team, away_team):
 
 st.title(f"{current_season} CBASE PEAR")
 st.caption(f"Ratings Updated {formatted_latest_date}")
-st.caption(f"Stats Updated {last_date}")
+st.caption(f"Stats Through Games {last_date}")
 
 st.divider()
 
