139275ce8b68b6f66352cfc94b819df2f5f3b0e2
Parslinator <wparsley2002@gmail.com>
2025-03-06 16:39:29 -0600

    reformatting some things

--- pages/5_College_Baseball.py
+++ pages/5_College_Baseball.py
@@ -597,9 +597,11 @@ st.subheader("Live CBASE Ratings and Resume")
 st.caption("Updated when page updates. Weekly rankings are taken Monday at 11 AM CST")
 modeling_stats_copy = modeling_stats.copy()
 modeling_stats_copy.set_index("Team", inplace=True)
+modeling_stats_copy['TSR'] = modeling_stats_copy['PRR']
 with st.container(border=True, height=440):
-    st.dataframe(modeling_stats_copy[['NET', 'RPI', 'PRR', 'RQI', 'SOS', 'RemSOS', 'Q1', 'Q2', 'Q3', 'Q4', 'Conference']], use_container_width=True)
-st.caption("NET - Mimicing the NCAA Evaluation Tool using PRR, RQI, SOS, RPI - PEAR's Attempted RPI, PRR - Power Rating Rank, RQI - Resume Quality Index, SOS - Strength of Schedule, RemSOS - Remaining Strength of Schedule")
+    st.dataframe(modeling_stats_copy[['NET', 'RPI', 'TSR', 'RQI', 'SOS', 'RemSOS', 'Q1', 'Q2', 'Q3', 'Q4', 'Conference']], use_container_width=True)
+st.caption("NET - Mimicing the NCAA Evaluation Tool using TSR, RQI, SOS")
+st.caption("RPI - PEAR's Attempted Ratings Percentage Index, TSR - Team Strength Rank, RQI - Resume Quality Index, SOS - Strength of Schedule, RemSOS - Remaining Strength of Schedule")
 
 st.divider()
 
