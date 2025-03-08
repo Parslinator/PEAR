e0043d1b6e21c4acd2408efaee1942e3d71558d9
Parslinator <wparsley2002@gmail.com>
2025-03-08 16:37:36 -0600

    changing resume quality calculation

--- baseball_power_rating.py
+++ baseball_power_rating.py
@@ -1090,6 +1090,7 @@ def calculate_resume_quality(group, bubble_team_rating):
             resume_quality += (1-win_prob)
         else:
             resume_quality -= win_prob
+    resume_quality = resume_quality / len(group)
     results.append({"Team": team, "resume_quality": resume_quality})
     return pd.DataFrame(results)
 
