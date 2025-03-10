6feb62762e4ac55e43cccaf82c05e5ef26f55598
Parslinator <wparsley2002@gmail.com>
2025-03-09 21:23:45 -0500

    doesn't work

--- pages/5_College_Baseball.py
+++ pages/5_College_Baseball.py
@@ -767,7 +767,6 @@ with st.form(key='net_change'):
     net_change = st.form_submit_button("Enter")
     if net_change:
         fig = team_net_tracker(team_name)
-        fig.set_size_inches(4, 4)
         st.pyplot(fig)
 
 st.divider()
