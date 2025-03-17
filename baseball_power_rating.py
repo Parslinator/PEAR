0913f6610cccaac9b1999850c128c2afbbdea9b4
Parslinator <wparsley2002@gmail.com>
2025-03-16 19:45:50 -0500

    lessen impact of last year rpi, sos

--- baseball_power_rating.py
+++ baseball_power_rating.py
@@ -531,7 +531,7 @@ bounds = [(0,1),
           (0,1),
           (0,1),
           (0,1),
-          (0,0.3),
+          (0,0.05),
           (0,1)]
 result = differential_evolution(objective_function, bounds, strategy='best1bin', maxiter=500, tol=1e-4, seed=42)
 optimized_weights = result.x
@@ -1219,7 +1219,7 @@ def calculate_net(weights):
     spearman_corr = stats_and_metrics[['NET', 'combined_rank']].corr(method='spearman').iloc[0,1]
 
     return -spearman_corr
-bounds = [(0.5,1), (0,.1)]  
+bounds = [(0.55,1), (0,.05)]  
 result = differential_evolution(calculate_net, bounds, strategy='best1bin', maxiter=500, tol=1e-4, seed=42)
 optimized_weights = result.x
 print("NET Calculation Weights:")
