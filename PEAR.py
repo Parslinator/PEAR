9f0dad61d71e36293abb2d4b391bf81dcd7ffcf5
Parslinator <wparsley2002@gmail.com>
2025-03-20 12:15:59 -0500

    adding year normalized ratings

--- PEAR.py
+++ PEAR.py
@@ -488,6 +488,325 @@ st.caption("MD - Most Deserving (PEAR's 'AP' Ballot), SOS - Strength of Schedule
 
 st.divider()
 
+team_data = pd.read_csv("./PEAR/normalized_power_rating_across_years.csv")
+
+def adjust_home_pr(home_win_prob):
+    return ((home_win_prob - 50) / 50) * 5
+
+def grab_team_elo_across_years(team, season):
+    season = int(season)
+    elo_ratings_list = [*ratings_api.get_elo_ratings(year=season, team=team)]
+    elo_ratings_dict = [dict(
+        team=e.team,
+        elo=e.elo
+    ) for e in elo_ratings_list]
+    elo_ratings = pd.DataFrame(elo_ratings_dict)       
+    return elo_ratings['elo'].values[0]
+
+def spreads_across_years(team1, team1_season, team2, team2_season, data, neutrality=False):
+    team1_season = int(team1_season)
+    team2_season = int(team2_season)
+    home_elo = grab_team_elo_across_years(team1, team1_season)
+    away_elo = grab_team_elo_across_years(team2, team2_season)
+    home_pr = data.loc[(data['team'] == team1) & (data['season'] == team1_season), 'norm_pr'].values[0]
+    away_pr = data.loc[(data['team'] == team2) & (data['season'] == team2_season), 'norm_pr'].values[0]
+    home_win_prob = round((10 ** ((home_elo - away_elo) / 400)) / ((10 ** ((home_elo - away_elo) / 400)) + 1) * 100, 2)
+    adjustment = adjust_home_pr(home_win_prob)
+    if neutrality:
+        spread = home_pr + adjustment - away_pr
+    else:
+        spread = 4.6 + home_pr + adjustment - away_pr
+    spread = round(spread,1)
+
+    if spread >= 0:
+        return f"{team1_season} {team1} -{spread}"
+    else:
+        return f"{team2_season} {team2} {spread}"
+    
+def teams_yearly_stats(team, data):
+    team_df = data[data['team'] == team]
+    team_df['OFF'] = team_df['offensive_rank']
+    team_df['DEF'] = team_df['defensive_rank']
+    team_df['MD'] = team_df['most_deserving']
+    team_df['Rating'] = team_df['power_rating']
+    team_df['Team'] = team_df['team']
+    team_df['CONF'] = team_df['conference']
+    team_df['ST'] = team_df['STM_rank']
+    team_df['PBR'] = team_df['PBR_rank']
+    team_df['DCE'] = team_df['DCE_rank']
+    team_df['DDE'] = team_df['DDE_rank']
+    team_df = team_df[['Season', 'Normalized Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE']]
+    return team_df
+
+st.sidebar.markdown(f"[Year Normalized Ratings](#year-normalized-ratings)", unsafe_allow_html=True)
+st.sidebar.markdown(f"[Team Specific Stats](#view-a-specific-teams-stats)", unsafe_allow_html=True)
+st.sidebar.markdown(f"[Spread Between Years](#calculate-spread-between-two-teams-from-different-years)", unsafe_allow_html=True)
+
+
+years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014]
+for year in years:
+    st.sidebar.markdown(f"[{year} Ratings](#{year}-ratings)", unsafe_allow_html=True)
+
+team_data.index += 1
+team_data['Team'] = team_data['team']
+team_data['Season'] = team_data['season'].astype(str)
+team_data['Normalized Rating'] = team_data['norm_pr']
+st.title("CFB Ratings Archive")
+st.markdown(f'<h2 id="year-normalized-ratings">Year Normalized Ratings</h2>', unsafe_allow_html=True)
+with st.container(border=True, height=440):
+    st.dataframe(team_data[['Team', 'Normalized Rating', 'Season']], use_container_width=True)
+
+st.divider()
+
+st.markdown(f'<h2 id="view-a-specific-teams-stats">View A Specific Teams Stats</h2>', unsafe_allow_html=True)
+with st.form(key='filter_team'):
+    team = st.selectbox("Team Filter", ["Select Team"] + list(sorted(team_data['team'].unique())))
+    filter_button = st.form_submit_button("Filter Team")
+    if filter_button:
+        st.dataframe(teams_yearly_stats(team, team_data), use_container_width=True)
+
+st.divider()
+
+st.markdown(f'<h2 id="calculate-spread-between-two-teams-from-different-years">Calculate Spread Between Two Teams From Different Years</h2>', unsafe_allow_html=True)
+with st.form(key='calculate_spread'):
+    away_team = st.selectbox("Away Team", ["Select Team"] + list(sorted(team_data['team'].unique())))
+    away_season = st.selectbox("Away Season", ["Select Season"] + list(sorted(team_data['season'].unique())))
+    home_team = st.selectbox("Home Team", ["Select Team"] + list(sorted(team_data['team'].unique())))
+    home_season = st.selectbox("Home Season", ["Select Season"] + list(sorted(team_data['season'].unique())))
+    neutrality = st.radio(
+        "Game Location",
+        ["Neutral Field", "On Campus"]
+    )
+    spread_button = st.form_submit_button("Calculate Spread")
+    if spread_button:
+        if neutrality == 'Neutral Field':
+            neutrality = True
+        else:
+            neutrality = False
+        st.write(spreads_across_years(home_team, home_season, away_team, away_season, team_data, neutrality))
+
+st.divider()
+
+st.markdown(f'<h2 id="2023-ratings">2023 Ratings</h2>', unsafe_allow_html=True)
+all_data = pd.read_csv("./PEAR/Data/y2023/team_data_week15.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2022/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2022-ratings">2022 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2021/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2021-ratings">2021 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2020/team_data_week17.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2020-ratings">2020 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2019/team_data_week17.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2019-ratings">2019 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2018/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2018-ratings">2018 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2017/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2017-ratings">2017 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2016/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2016-ratings">2016 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2015/team_data_week16.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2015-ratings">2015 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
+all_data = pd.read_csv("./PEAR/Data/y2014/team_data_week17.csv")
+all_data.rename(columns={"offensive_rank": "Offense"}, inplace=True)
+all_data.rename(columns={"defensive_rank": "Defense"}, inplace=True)
+st.markdown(f'<h2 id="2014-ratings">2014 Ratings</h2>', unsafe_allow_html=True)
+all_data['OFF'] = all_data['Offense']
+all_data['DEF'] = all_data['Defense']
+all_data['MD'] = all_data['most_deserving']
+all_data['Rating'] = all_data['power_rating']
+all_data['Team'] = all_data['team']
+all_data['CONF'] = all_data['conference']
+all_data['ST'] = all_data['STM_rank']
+all_data['PBR'] = all_data['PBR_rank']
+all_data['DCE'] = all_data['DCE_rank']
+all_data['DDE'] = all_data['DDE_rank']
+all_data.index = all_data.index + 1
+with st.container(border=True, height=440):
+    st.dataframe(all_data[['Team', 'Rating', 'MD', 'SOS', 'SOR', 'OFF', 'DEF', 'ST', 'PBR', 'DCE', 'DDE', 'CONF']], use_container_width=True)
+st.caption("MD - Most Deserving (ESCAPE's 'AP' Ballot), SOS - Strength of Schedule, SOR - Strength of Record, OFF - Offense, DEF - Defense, ST - Special Teams, PBR - Penalty Burden Ratio, DCE - Drive Control Efficiency, DDE - Drive Disruption Efficiency")
+# , MD - Most Deserving Rankings
+
+st.divider()
+
 # st.markdown("General Info for PEAR v2")
 # st.caption(f"SU Since Week 9: {SU}%")
 # st.caption(f"ATS Since Week 9: {ATS}%")
