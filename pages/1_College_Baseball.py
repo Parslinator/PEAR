9357e974056104369cdbb0a83a122e418510496b
Parslinator <wparsley2002@gmail.com>
2025-05-13 17:01:54 -0500

    Ivy League at home bracket

--- pages/1_College_Baseball.py
+++ pages/1_College_Baseball.py
@@ -1626,7 +1626,9 @@ def simulate_conference_tournaments(schedule_df, stats_and_metrics, num_simulati
         seed_order = [team for team, _ in team_win_pcts[:4]]
         if conference == 'Ivy League':
             seed_order = ['Yale', 'Columbia', 'Penn', 'Harvard']
-        output = run_simulation(seed_order[0], seed_order[1], seed_order[2], seed_order[3], stats_and_metrics)
+            output = run_simulation(seed_order[0], seed_order[1], seed_order[2], seed_order[3], stats_and_metrics, num_simulations, True)
+        else:
+            output = run_simulation(seed_order[0], seed_order[1], seed_order[2], seed_order[3], stats_and_metrics, num_simulations)
         final_df = pd.DataFrame(list(output.items()), columns=["Team", "Win Tournament"])
         final_df["Win Tournament"] = final_df["Win Tournament"] * 100
         fig = plot_tournament_odds_table(final_df, 0.5, conference, 0.143, 0.12, 0.4)
