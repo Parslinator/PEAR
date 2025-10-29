12a79912fd787a0eeb09af63972e9fb5effd18ce
Parslinator <wparsley2002@gmail.com>
2025-10-29 14:07:25 -0500

    updating next. config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', '129.212.189.128'],
     remotePatterns: [
       {
         protocol: 'http',
@@ -9,8 +15,12 @@ const nextConfig = {
         port: '8000',
         pathname: '/api/image/**',
       },
+      {
+        protocol: 'http',
+        hostname: '129.212.189.128',
+        pathname: '/api/image/**',
+      },
     ],
   },
 }
-
 module.exports = nextConfig
